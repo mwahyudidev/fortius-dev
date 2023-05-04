d8de4a7a335b866c30717e9e2ddd75f60752ba17
mwahyudi <wahyudi@prakasagroup.com>
2023-05-04 16:54:22 +0700

    Develop Page Payment Method

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Switch } from 'react-router-dom';
+// We will create these two pages in a moment
+import Payment from './pages/Payment';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Switch>
+      <Route exact path="/" component={Payment} />
+      {/* <Route path="/:id" component={UserPage} /> */}
+    </Switch>
+  )
 }
-
-export default App;
